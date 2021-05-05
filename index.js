feac2c8179171c527e2a1c9fe758f5cce6001cf3
Danny Rivera <samboard@hotmail.com>
2021-05-05 10:40:42 -0400

    commit tree 774e9e695a57a29cd300b6131e73f7ba0e78a0ff

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+// Make sure you remove the <main> with id 'main': expected <main id="main"></main> to not exist
+document.querySelector("main#main").remove();
+
+// has a 'newHeader' variable that points to node 'h1#victory'
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+// has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+newHeader.innerHTML = "<p>${YOUR-NAME} is the champion!</p>";
